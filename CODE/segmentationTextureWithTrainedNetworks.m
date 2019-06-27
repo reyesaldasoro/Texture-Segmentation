19e0f585eee329adf24d3b76169b4fa60eb7b06d
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-06-27 16:43:41 +0100

    Update segmentationTextureWithTrainedNetworks.m
    update for a series of results

--- CODE/segmentationTextureWithTrainedNetworks.m
+++ CODE/segmentationTextureWithTrainedNetworks.m
@@ -21,31 +21,38 @@ load randenData
 clear resRanden stdsRanden meansRanden fname ind edge error*
 
 %%
-dir_nets = dir ('Network_Case*');
+%dir_nets = dir ('Network_Case*');
 
-%% Create Network
-%load textureNet2
-% select one of the composite images
-for currentCase                 = 1:9
-    [rows2,cols2,numClasses]      = size(trainRanden{currentCase});
-    [rows,cols,numClasses2]      = size(dataRanden{currentCase});
-    %
-    load (dir_nets(currentCase).name);
-    C = semanticseg(uint8(dataRanden{currentCase}),net);
-    B = labeloverlay(uint8(dataRanden{currentCase}), C);
-    figure(currentCase)
-    imagesc(B)
-    % Convert from semantic to numeric
-    result = zeros(rows,cols);
-    for counterClass=1:numClasses
-        %strcat('T',num2str(counterClass))
-        %result = result + counterClass*((C==strcat('T',num2str(counterClass))));    
-        result = result +(counterClass*(C==strcat('T',num2str(counterClass))));
+%% Evaluate Networks
+for counterOptions =1:3
+    switch counterOptions
+        case 1
+            dir_nets = dir ('Network_Case_?.mat');
+        case 2
+            dir_nets = dir ('Network_Case_?B.mat');
+        case 3
+            dir_nets = dir ('Network_Case_?C.mat');
     end
-    figure(10+currentCase)
-    imagesc(result==maskRanden{currentCase})
-    accuracy=sum(sum(result==maskRanden{currentCase}))/rows/cols;
-    disp([currentCase accuracy])
-    
-    
+    for currentCase                 = 1:9
+        % select one of the composite images
+        [rows2,cols2,numClasses]      = size(trainRanden{currentCase});
+        [rows,cols,numClasses2]      = size(dataRanden{currentCase});
+        %
+        load (dir_nets(currentCase).name);
+        C = semanticseg(uint8(dataRanden{currentCase}),net);
+        B = labeloverlay(uint8(dataRanden{currentCase}), C);
+        %figure(currentCase)
+        %imagesc(B)
+        % Convert from semantic to numeric
+        result = zeros(rows,cols);
+        for counterClass=1:numClasses
+            %strcat('T',num2str(counterClass))
+            %result = result + counterClass*((C==strcat('T',num2str(counterClass))));
+            result = result +(counterClass*(C==strcat('T',num2str(counterClass))));
+        end
+        %figure(10*counterOptions+currentCase)
+        %imagesc(result==maskRanden{currentCase})
+        accuracy(counterOptions,currentCase)=sum(sum(result==maskRanden{currentCase}))/rows/cols;
+        %disp([currentCase accuracy])
+    end
 end
