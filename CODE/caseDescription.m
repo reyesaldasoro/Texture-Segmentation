de06da729ba0b84923dac23875ce8e62d9807450
CCRA <ccr22@cm3-1-01-83.managed.city.ac.uk>
2019-07-04 16:26:52 +0100

    Create caseDescription.m

---
+++ CODE/caseDescription.m
@@ -0,0 +1,21 @@
+
+% CASE                          MiniBatchSize   
+% 1         sgdm        H       64              1E-3
+% 2         adam        H       64              1E-3
+% 3         rmsprop     H       64              1E-3
+
+% 4         sgdm        HVD     64              1E-3
+% 5         adam        HVD     64              1E-3
+% 6         rmsprop     HVD     64              1E-3
+
+% 7         sgdm        HVD     64              1E-3
+% 8         adam        HVD     64              1E-3
+% 9         rmsprop     HVD     64              1E-3
+
+% 10        sgdm        HVD     64              1E-4
+% 11        adam        HVD     64              1E-4
+% 12        rmsprop     HVD     64              1E-4
+
+% 13        sgdm        HVD     16              1E-3
+% 14        adam        HVD     16              1E-3    
+% 15        rmsprop     HVD     16              1E-3    
