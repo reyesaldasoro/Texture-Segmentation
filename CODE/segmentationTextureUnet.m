ac755819265392b9752a37dc04f60f4555b78e74
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-07-10 18:04:25 +0100

    Update segmentationTextureUnet.m

--- CODE/segmentationTextureUnet.m
+++ CODE/segmentationTextureUnet.m
@@ -12,7 +12,7 @@ if strcmp(filesep,'/')
 else
     % running in windows
     %    dataSetDir ='D:\Acad\GitHub\Texture-Segmentation\CODE';
-    dataSetDir =  'D:\OneDrive - City, University of London\Acad\Research\texture\Horiz_Vert_Diag\';    
+    dataSetDir =  'D:\OneDrive - City, University of London\Acad\Research\texture\Horiz_Vert_Diag\';
     cd ('D:\Acad\GitHub\Texture-Segmentation\CODE')
 end
 %%
@@ -22,94 +22,158 @@ load randenData
 % maskRanden    -  cell with the masks for each of the composite images
 clear resRanden stdsRanden meansRanden fname ind edge error*
 
-%% Create Network
-%load textureNet2
-% select one of the composite images
-for caseEncoder =1%:3
-    switch caseEncoder
-        case 1
-            dir_nets        = dir ('Network_Case_?A.mat');
-            typeEncoder     = 'sgdm';
-            nameEncoder     = 'A';
-        case 2
-            dir_nets        = dir ('Network_Case_?B.mat');
-            typeEncoder     = 'adam';
-            nameEncoder     = 'B';
-        case 3
-            dir_nets        = dir ('Network_Case_?C.mat');
-            typeEncoder     = 'rmsprop';
-            nameEncoder     = 'C';
+accuracy(3,9,3) = 0;
+%% Loop for training and segmentation
+% select one of the composite images of the randen cases, there are 9 images
+for currentCase                 = 1%:9
+    % dimensions of the data
+    [rows,cols,numClasses]      = size(trainRanden{currentCase});
+    % location of the training data data and labels are stored as pairs of textures arranged in Horizontal,
+    % Vertical and Diagonal pairs of class 1-2, 1-3, 1-4 ... 2-1, 2-3,...
+    imageDir                    = fullfile(dataSetDir,strcat('trainingImages',filesep,'Case_',num2str(currentCase)));
+    labelDir                    = fullfile(dataSetDir,strcat('trainingLabels',filesep,'Case_',num2str(currentCase)));
+    imageSize                   = [rows cols];
+    encoderDepth                = 4;
+    % These are the data stores with the training pairs and training labels
+    % They can be later used to create montages of the pairs.
+    imds                        = imageDatastore(imageDir);
+    imds2                       = imageDatastore(labelDir);
+    % The class names are a sequence of options for the textures, e.g.
+    % classNames = ["T1","T2","T3","T4","T5"];
+    clear classNames
+    for counterClass=1:numClasses
+        classNames(counterClass) = strcat("T",num2str(counterClass));
     end
+    % The labels are simply the numbers of the textures, same numbers
+    % as with the classNames. For randen examples, these vary 1-5, 1-16, 1-10
+    labelIDs                    = (1:numClasses);
+    pxds                        = pixelLabelDatastore(labelDir,classNames,labelIDs);
     
-    for currentCase                 = 1%:9
-        [rows,cols,numClasses]      = size(trainRanden{currentCase});
-        imageDir = fullfile(dataSetDir,strcat('trainingImages',filesep,'Case_',num2str(currentCase)));
-        labelDir = fullfile(dataSetDir,strcat('trainingLabels',filesep,'Case_',num2str(currentCase)));
-        
-        imageSize = [rows cols];
-        %numClasses = 5;
-        encoderDepth = 4;
-        % These are the data stores with the training pairs and training labels
-        % They can be later used to create montages of the pairs.
-        imds  = imageDatastore(imageDir); 
-        imds2 = imageDatastore(labelDir);
-
-        % The class names are a sequence of options for the textures, e.g.
-        % classNames = ["T1","T2","T3","T4","T5"];
-        clear classNames
-        for counterClass=1:numClasses
-            classNames(counterClass) = strcat("T",num2str(counterClass));
+    % try with different encoders
+    for caseEncoder =1%:3
+        switch caseEncoder
+            case 1
+                dir_nets        = dir ('Network_Case_?A.mat');
+                typeEncoder     = 'sgdm';
+                nameEncoder     = '1';
+            case 2
+                dir_nets        = dir ('Network_Case_?B.mat');
+                typeEncoder     = 'adam';
+                nameEncoder     = '2';
+            case 3
+                dir_nets        = dir ('Network_Case_?C.mat');
+                typeEncoder     = 'rmsprop';
+                nameEncoder     = '3';
         end
-        % The labels are simply the numbers of the textures, same numbers 
-        % as with the classNames. For randen examples, these vary 1-5, 1-16, 1-10
-        labelIDs   = (1:numClasses);
-        
-        pxds = pixelLabelDatastore(labelDir,classNames,labelIDs);
-        
         
         % Definition of the network to be trained.
         numFilters                  = 64;
         filterSize                  = 3;
-        %numClasses = 5;
-        layers = [
-            imageInputLayer([32 32 1])
-            convolution2dLayer(filterSize,numFilters,'Padding',1)
-            reluLayer()
-            maxPooling2dLayer(2,'Stride',2)
-            convolution2dLayer(filterSize,numFilters,'Padding',1)
-            reluLayer()
-            maxPooling2dLayer(2,'Stride',2)
-            convolution2dLayer(filterSize,numFilters,'Padding',1)
+        
+        for numLayersNetwork =1:3
+            switch numLayersNetwork
+                case 1
+                    layers = [
+                        imageInputLayer([32 32 1])
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
                         reluLayer()
-            maxPooling2dLayer(2,'Stride',2)
-            convolution2dLayer(filterSize,numFilters,'Padding',1)
-            reluLayer()
-            transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
-            convolution2dLayer(1,numClasses);         
-            transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
-            convolution2dLayer(1,numClasses);          
-            transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
-            convolution2dLayer(1,numClasses);
-            softmaxLayer()
-            pixelClassificationLayer()
-            ];
-        
-        opts = trainingOptions(typeEncoder, ...
-            'InitialLearnRate',1e-3, ...
-            'MaxEpochs',3, ...
-            'MiniBatchSize',64);
-
-        trainingData        = pixelLabelImageDatastore(imds,pxds);
-        nameNet             = strcat(dataSetDir,'Network_Case_',num2str(currentCase),nameEncoder);
-        disp(nameNet)
-        net                 = trainNetwork(trainingData,layers,opts);
-
-        %save(nameNet,'net')
-        
-        %
-        C = semanticseg(uint8(dataRanden{currentCase}),net);
-        B = labeloverlay(uint8(dataRanden{currentCase}), C);
-        figure
-        imagesc(B)
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        softmaxLayer()
+                        pixelClassificationLayer()
+                        ];
+                    nameLayers     = '10';
+                case 2
+                    layers = [
+                        imageInputLayer([32 32 1])
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        softmaxLayer()
+                        pixelClassificationLayer()
+                        ];
+                    
+                    nameLayers     = '15';
+                case 3
+                    layers = [
+                        imageInputLayer([32 32 1])
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        maxPooling2dLayer(2,'Stride',2)
+                        convolution2dLayer(filterSize,numFilters,'Padding',1)
+                        reluLayer()
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+                        convolution2dLayer(1,numClasses);
+                        softmaxLayer()
+                        pixelClassificationLayer()
+                        ];
+                    
+                    nameLayers     = '20';
+            end
+            
+            
+            opts = trainingOptions(typeEncoder, ...
+                'InitialLearnRate',1e-3, ...
+                'MaxEpochs',100, ...
+                'MiniBatchSize',64);
+            
+            trainingData        = pixelLabelImageDatastore(imds,pxds);
+            nameNet             = strcat(dataSetDir,'Network_Case_',num2str(currentCase),'_Enc_',nameEncoder,'_numL_',nameLayers);
+            disp(nameNet)
+            net                 = trainNetwork(trainingData,layers,opts);
+            
+            save(nameNet,'net')
+            
+            %
+            C = semanticseg(uint8(dataRanden{currentCase}),net);
+            B = labeloverlay(uint8(dataRanden{currentCase}), C);
+            figure
+            imagesc(B)
+            
+            % Convert from semantic to numeric
+            result = zeros(rows,cols);
+            for counterClass=1:numClasses
+                %strcat('T',num2str(counterClass))
+                %result = result + counterClass*((C==strcat('T',num2str(counterClass))));
+                result = result +(counterClass*(C==strcat('T',num2str(counterClass))));
+            end
+            %figure(10*counterOptions+currentCase)
+            %imagesc(result==maskRanden{currentCase})
+            accuracy(numLayersNetwork,currentCase,caseEncoder)=sum(sum(result==maskRanden{currentCase}))/rows/cols;
+         end
     end
 end
+
+misclassification = 100*(1-accuracy);
